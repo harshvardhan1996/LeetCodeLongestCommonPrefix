8d709d94f1a83b160a3ef18f9e5caafa7d40f4cf
harshvardhan1996 <harshamazon1996@gmail.com>
2018-06-17 02:51:48 +0530

    Longest Common Prefix

--- LeetCodeLongestCommonPrefix/Program.cs
+++ LeetCodeLongestCommonPrefix/Program.cs
@@ -10,6 +10,40 @@ namespace LeetCodeLongestCommonPrefix
     {
         static void Main(string[] args)
         {
+            string[] strs = { "leets", "leetcode", "leet", "leeds" };
+            Console.WriteLine("Longest Common Prefix is : " + LongestCommonPrefix(strs));
+            Console.ReadKey();
+        }
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            if (strs.Length == 0) return "";
+            string longest = "";
+            int minimumLength = MinimumLengthString(strs);
+
+
+            for (int i = 0; i < minimumLength; i++)
+            {
+                char current = strs[0][i];
+                for (int j = 1; j < strs.Length; j++)
+                {
+                    if (strs[j][i] != current)
+                        return longest;
+                }
+                longest += current;
+
+            }
+            return longest;
+        }
+        public static int MinimumLengthString(string[] strs)
+        {
+            int min = strs[0].Length;
+            for (int i = 1; i < strs.Length; i++)
+            {
+                if (strs[i].Length < min)
+                    min = strs[i].Length;
+            }
+
+            return min;
         }
     }
 }
